da322913975227575258199f765d2e00288d9071
yourgitusername <yourgitmail>
2022-03-10 13:50:10 -0500

    second commit

---
+++ Test.py
@@ -0,0 +1,14 @@
+from django.http  import response 
+from django.test import SimpleTestCase
+
+class HomePageTests(SimpleTestCase):
+
+    def test_admin_page_status_code(self):
+        print("Verify admin Page")
+        response=self.client.get('/admnin')
+        self.assertEquals(response.status_code,301)
+    
+    def test_about_page_status_code(self):
+        print("verify not exit about page") 
+        response=self.client.get('/about')
+        self.assertEquals(response.status_code,404)
